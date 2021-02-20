09c6680fba5301d6423f971bd9116a3c922518cb
Dmytro <dmytro.v.levchenko@gmail.com>
2021-02-20 13:05:57 +0200

    3rd commit - add twig in project

--- index.php
+++ index.php
@@ -1,90 +1,31 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <title>Hello, world!</title>
-</head>
-<body>
-<header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Blog</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    <a class="nav-link" href="#">About</a>
-                </div>
-            </div>
-        </div>
-    </nav>
-</header>
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
-<div class="container">
-    <h1 class="mb-3 mt-3 text-center">Welcome my blog!</h1>
-    <div class="container row justify-content-center mb-3">
-        <div class="card p-1" style="width: 18rem;">
-            <img src="https://dummyimage.com/300x200/0f15d1/fff" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-        <div class="card p-1" style="width: 18rem;">
-            <img src="https://dummyimage.com/300x200/0f15d1/fff" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-        <div class="card p-1" style="width: 18rem;">
-            <img src="https://dummyimage.com/300x200/0f15d1/fff" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    </div>
+require __DIR__ . '/vendor/autoload.php';
 
-    <div class="container row justify-content-center mb-3">
-        <div class="card p-1" style="width: 18rem;">
-            <img src="https://dummyimage.com/300x200/0f15d1/fff" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-        <div class="card p-1" style="width: 18rem;">
-            <img src="https://dummyimage.com/300x200/0f15d1/fff" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-        <div class="card p-1" style="width: 18rem;">
-            <img src="https://dummyimage.com/300x200/0f15d1/fff" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    </div>
-</div>
+// include twig
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
 
+$app = AppFactory::create();
+$app->setBasePath("/php-blog");
 
+$app->get('/', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
+    return $response;
+});
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-</body>
-</html>
+$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('about.twig', [
+        'name' => 'Dima'
+    ]);
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->run();
